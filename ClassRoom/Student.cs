de2ff12ef320be454752399728ab91a670f3a921
Nohely <ngedeon14@yahoo.com>
2021-09-02 09:45:55 +0200

    Task 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        public string name { get; set; }
+        public int birthMonth { get; set; }
+        public int birthDay { get; set; }
+
+        public Student (string Name, int BirthMonth, int BirthDay)
+        {
+            name = Name;
+            birthMonth = BirthMonth;
+            birthDay = BirthDay;
+        }
+
+
+    }
+}
