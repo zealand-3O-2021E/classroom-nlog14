7af7459d577ddacd58985cb9e3615631528013de
Nohely <ngedeon14@yahoo.com>
2021-09-02 09:51:13 +0200

    Task 2

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        public string className { get; set; }
+        public List<Student> ClassList { get; set; }
+        public DateTime semesterStart { get; set; }
+
+        public ClassRoom ()
+        { }
+    }
+}
